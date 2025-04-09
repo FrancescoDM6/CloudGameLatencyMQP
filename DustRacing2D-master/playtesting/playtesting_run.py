16b3566849701478b81ff00ed1cac1275b3bd726
FrancescoDM6 <112992086+FrancescoDM6@users.noreply.github.com>
2025-04-09 14:44:10 -0400

    player 14

--- DustRacing2D-master/playtesting/playtesting_run.py
+++ DustRacing2D-master/playtesting/playtesting_run.py
@@ -17,7 +17,7 @@ from pynput.keyboard import Controller, Key
 
 LatinSquare = "/home/claypool/Desktop/CloudGameLatencyMQP/Latin Square Setup(1).csv"
 
-player_id = 13
+player_id = 15
 
 class GameTestConfig:
    
